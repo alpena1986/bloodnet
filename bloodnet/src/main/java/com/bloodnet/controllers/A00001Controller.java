305cb4cfe8cdf449bca07e3e0285b10781bb6dcb
alpena <alpena@192.168.1.7>
2017-03-21 20:14:37 +0900

    mysql数据连接部分更新

--- bloodnet/src/main/java/com/bloodnet/controllers/A00001Controller.java
+++ bloodnet/src/main/java/com/bloodnet/controllers/A00001Controller.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
@@ -18,9 +20,12 @@ import com.bloodnet.controllers.com.BaseController;
 @Controller
 public class A00001Controller extends BaseController {
     
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
     @RequestMapping(value="/admin", method=RequestMethod.GET)
     public String init(Model model, HttpSession httpSession,  Locale locale) throws Exception {
     	
+    	jdbcTemplate.execute("select 1");
         return "A00001";
     }
 }
