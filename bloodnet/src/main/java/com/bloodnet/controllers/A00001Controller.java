5616ae8dd0b5141f07e5274317f74896aee5a038
alpena <alpena@192.168.1.7>
2017-04-02 11:31:15 +0900

    update

--- bloodnet/src/main/java/com/bloodnet/controllers/A00001Controller.java
+++ bloodnet/src/main/java/com/bloodnet/controllers/A00001Controller.java
@@ -9,9 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import com.bloodnet.controllers.com.BaseController;
 import com.bloodnet.form.A00001Form;
+import com.bloodnet.lib.Profile;
 import com.bloodnet.services.A00001Service;
 
 
@@ -22,18 +24,22 @@ public class A00001Controller extends BaseController {
 	private JdbcTemplate jdbcTemplate;
 	
 	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
 	private A00001Service a00001Service;
 	
     @RequestMapping(value="/1", method=RequestMethod.GET)
     public String init(Model model, @ModelAttribute A00001Form form , HttpSession httpSession) throws Exception {
     	
     	jdbcTemplate.execute("select 1");
+    	Profile p  = restTemplate.getForObject("http://localhost:8080/bloodnetapi/1", Profile.class);
+    	p.getId();
         return "A00001";
     }
     
     @RequestMapping(value="/1/register", method=RequestMethod.POST)
     public String register(Model model,@ModelAttribute A00001Form form ,HttpSession httpSession) throws Exception {
-    	
     	a00001Service.createHuman(form.getEmail(), form.getFamilyName(), form.getFirstName(), form.getSex(), form.getBirthdate());
     	model.addAttribute("message", "登录成功");
         return "A00001";
